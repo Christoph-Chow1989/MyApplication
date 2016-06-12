b57a04d19f947fc817ee3c46be70337e98e30424
MarcoChow <100360258@qq.com>
2016-06-12 14:13:53 +0800

    2016年6月12日14:13:47

--- MVP_MySample_demo/src/main/java/com/marco/www/mvp_mysample_demo/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.marco.www.mvp_mysample_demo;
-
-/**
- * Created by pc on 2016/6/7.
- */
-public class Constant
-{
-    public static final String ROAD_URL = "http://116.236.170.106:9001/RemoteData/InformationHandler.ashx";
-}
--- MVP_MySample_demo/src/main/java/com/marco/www/mvp_mysample_demo/model/RoadDataListModel.java
+++
@@ -1,9 +0,0 @@
-package com.marco.www.mvp_mysample_demo.model;
-
-/**
- * Created by pc on 2016/6/7.
- */
-public interface RoadDataListModel
-{
-    void GetRoadList(String road_url, String type, RoadDataListModelImpl.GetRoadDataListenter listener);
-}
